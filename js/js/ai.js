6281052173ed65f326e918cc3dd74337064c5e75
quantora396 <quantora396@gmail.com>
2026-07-01 00:07:58 +0330

    Update ai.js

--- js/js/ai.js
+++ js/js/ai.js
@@ -1,1 +1,41 @@
-// Quantora AI
+/*
+====================================
+        Quantora AI Engine v2
+====================================
+*/
+
+const ai = {
+
+    score: 0,
+
+    confidence: "Low",
+
+    signal: "WAIT",
+
+    technicalScore: 0,
+
+    fundamentalScore: 0,
+
+    smartMoneyScore: 0,
+
+    riskScore: 0,
+
+    reasons: []
+
+};
+
+function resetAI() {
+
+    ai.score = 0;
+
+    ai.technicalScore = 0;
+
+    ai.fundamentalScore = 0;
+
+    ai.smartMoneyScore = 0;
+
+    ai.riskScore = 0;
+
+    ai.reasons = [];
+
+}
