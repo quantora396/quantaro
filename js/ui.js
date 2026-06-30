866dc12e767d714d3d392fe434d6aad57fdf9c78
quantora396 <quantora396@gmail.com>
2026-06-30 23:19:36 +0330

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,34 @@
-// Quantora UI
+/*
+====================================
+    Quantora UI
+====================================
+*/
+
+function updateUI() {
+
+    if (market.btc !== null) {
+        document.getElementById("btc-price").innerHTML =
+            "$" + Number(market.btc).toLocaleString("en-US");
+    }
+
+    if (market.gold !== null) {
+        document.getElementById("gold-price").innerHTML =
+            "$" + Number(market.gold).toLocaleString("en-US");
+    }
+
+    if (market.btcTrend) {
+        document.getElementById("btc-trend").innerHTML =
+            market.btcTrend;
+    }
+
+    if (market.goldTrend) {
+        document.getElementById("gold-trend").innerHTML =
+            market.goldTrend;
+    }
+
+    if (market.recommendation) {
+        document.getElementById("recommendation").innerHTML =
+            market.recommendation;
+    }
+
+}
