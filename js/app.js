408484db6cf8b3722ec8c173c1d48c7cec7cde16
quantora396 <quantora396@gmail.com>
2026-06-30 23:22:32 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-// Quantora App
+/*
+====================================
+        Quantora App
+====================================
+*/
+
+async function startQuantora() {
+
+    await refreshMarketData();
+
+    runAI();
+
+    updateUI();
+
+}
+
+startQuantora();
+
+// هر 30 ثانیه بروزرسانی
+setInterval(async () => {
+
+    await refreshMarketData();
+
+    runAI();
+
+    updateUI();
+
+}, 30000);
