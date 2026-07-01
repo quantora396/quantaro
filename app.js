61f2f72902a10c0b909243786572eab5ba46cfeb
quantora396 <quantora396@gmail.com>
2026-07-01 14:55:46 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 
+/*
+====================================
+        Quantora App
+====================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadMarket();
+
+    setInterval(() => {
+        loadMarket();
+    }, 60000);
+
+});
